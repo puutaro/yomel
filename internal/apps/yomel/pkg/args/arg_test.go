de736e9385a33542ab6265cbaaa8393e647e48fa
puutaro <hoperstep@gmail.com>
2026-07-24 23:24:45 +0900

    add test case

--- internal/apps/yomel/pkg/args/arg_test.go
+++ internal/apps/yomel/pkg/args/arg_test.go
@@ -11,15 +11,18 @@ import (
 
 func Test_GenArgTable(t *testing.T) {
 	tests := []struct {
-		name  string   // テストケースの名前
-		input []string // 入力する引数
+		name  string
+		input []string
 		want  []args.ArgTable
 	}{
 		{
 			name: "make argTable from args",
 			input: []string{
-				"yomoel",
+				"yomel",
 				"--log",
+				"--no-log",
+				"--version",
+				"--help",
 				"--log-filter", "grep log",
 				"--err-log-filter", "grep err_log",
 				"stage", "test1",
@@ -28,10 +31,12 @@ func Test_GenArgTable(t *testing.T) {
 				"--opt", "a",
 				"--val", "--s", "aaa",
 				"--opt", "b",
-				"--val", "--s", "bbb",
+				"--val", "--n", "bbb",
 				"--lop", "c",
 				"--arg", "--s", "awsawsaws1",
-				"--arg", "--s", "awsawsaws2",
+				"--arg", "--n", "awsawsaws2",
+				"--single",
+				"--no-quote",
 				"-svc", "a3api",
 				"--opt", "e",
 				"--val", "--s", "eeee",
@@ -52,108 +57,118 @@ func Test_GenArgTable(t *testing.T) {
 				"--arg", "/aa/bb/",
 			},
 			want: []args.ArgTable{
-				// --log
+				// parse --log option
 				{StageNo: 0, IsLog: true},
-				// --log-filter "grep log"
+				// parse --no-log option
+				{StageNo: 0, IsNoLog: true},
+				// parse --version option
+				{StageNo: 0, IsVersion: true},
+				// parse --help option
+				{StageNo: 0, IsHelp: true},
+				// parse --log-filter option with pattern
 				{StageNo: 0, IsLogFilter: true},
 				{StageNo: 0, Str: testutil.Ptr("grep log")},
-				// --err-log-filter "grep err_log"
+				// parse --err-log-filter option with pattern
 				{StageNo: 0, IsErrLogFilter: true},
 				{StageNo: 0, Str: testutil.Ptr("grep err_log")},
 
-				// stage "test1"
+				// parse stage definition for test1
 				{StageNo: 1, IsStage: true},
 				{StageNo: 1, Str: testutil.Ptr("test1")},
-				// --log-filter "grep \"log aws1\""
+				// parse stage level log-filter
 				{StageNo: 1, IsLogFilter: true},
 				{StageNo: 1, Str: testutil.Ptr("grep \"log aws1\"")},
-				// -cmd "aws"
+				// parse command option
 				{StageNo: 1, IsCmd: true},
 				{StageNo: 1, Str: testutil.Ptr("aws")},
-				// --opt "a"
+				// parse option a
 				{StageNo: 1, IsOpt: true},
 				{StageNo: 1, Str: testutil.Ptr("a")},
-				// --val --s "aaa"
+				// parse value with single quote signal
 				{StageNo: 1, IsValue: true},
 				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
 				{StageNo: 1, Str: testutil.Ptr("aaa")},
-				// --opt "b"
+				// parse option b
 				{StageNo: 1, IsOpt: true},
 				{StageNo: 1, Str: testutil.Ptr("b")},
-				// --val --s "bbb"
+				// parse value with no quote signal
 				{StageNo: 1, IsValue: true},
-				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
+				{StageNo: 1, QuoteTypeSignal: args.NoQuote},
 				{StageNo: 1, Str: testutil.Ptr("bbb")},
-				// --lop "c"
+				// parse long option c
 				{StageNo: 1, IsLopt: true},
 				{StageNo: 1, Str: testutil.Ptr("c")},
-				// --arg --s "awsawsaws1"
+				// parse argument with single quote
 				{StageNo: 1, IsArg: true},
 				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
 				{StageNo: 1, Str: testutil.Ptr("awsawsaws1")},
-				// --arg --s "awsawsaws2"
+				// parse argument with no quote
 				{StageNo: 1, IsArg: true},
-				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
+				{StageNo: 1, QuoteTypeSignal: args.NoQuote},
 				{StageNo: 1, Str: testutil.Ptr("awsawsaws2")},
-				// -svc "a3api"
+				// parse single quote modifier
+				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
+				// parse no-quote modifier
+				{StageNo: 1, QuoteTypeSignal: args.NoQuote},
+				// parse service option
 				{StageNo: 1, IsSvc: true},
 				{StageNo: 1, Str: testutil.Ptr("a3api")},
-				// --opt "e"
+				// parse option e
 				{StageNo: 1, IsOpt: true},
 				{StageNo: 1, Str: testutil.Ptr("e")},
-				// --val --s "eeee"
+				// parse value for option e
 				{StageNo: 1, IsValue: true},
 				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
 				{StageNo: 1, Str: testutil.Ptr("eeee")},
-				// --opt "f"
+				// parse option f
 				{StageNo: 1, IsOpt: true},
 				{StageNo: 1, Str: testutil.Ptr("f")},
-				// --val --s "ffff"
+				// parse value for option f
 				{StageNo: 1, IsValue: true},
 				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
 				{StageNo: 1, Str: testutil.Ptr("ffff")},
-				// --arg --s "svcsvcsvc1"
+				// parse argument svcsvcsvc1
 				{StageNo: 1, IsArg: true},
 				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
 				{StageNo: 1, Str: testutil.Ptr("svcsvcsvc1")},
-				// --arg --s "svcsvcsvc2"
+				// parse argument svcsvcsvc2
 				{StageNo: 1, IsArg: true},
 				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
 				{StageNo: 1, Str: testutil.Ptr("svcsvcsvc2")},
-				// -act "list-objects"
+				// parse action option
 				{StageNo: 1, IsAct: true},
 				{StageNo: 1, Str: testutil.Ptr("list-objects")},
-				// --lop "s"
+				// parse long option s
 				{StageNo: 1, IsLopt: true},
 				{StageNo: 1, Str: testutil.Ptr("s")},
-				// --val --s "sss"
+				// parse value for long option s
 				{StageNo: 1, IsValue: true},
 				{StageNo: 1, QuoteTypeSignal: args.SingleQuote},
 				{StageNo: 1, Str: testutil.Ptr("sss")},
-				// --lop "t"
+				// parse long option t
 				{StageNo: 1, IsLopt: true},
 				{StageNo: 1, Str: testutil.Ptr("t")},
-				// --val --n "ttt"
+				// parse value for long option t
 				{StageNo: 1, IsValue: true},
 				{StageNo: 1, QuoteTypeSignal: args.NoQuote},
 				{StageNo: 1, Str: testutil.Ptr("ttt")},
-				// --arg "agagagaga1"
+				// parse raw argument 1
 				{StageNo: 1, IsArg: true},
 				{StageNo: 1, Str: testutil.Ptr("agagagaga1")},
-				// --arg "agagagaga2"
+				// parse raw argument 2
 				{StageNo: 1, IsArg: true},
 				{StageNo: 1, Str: testutil.Ptr("agagagaga2")},
 
-				// stage "sed"
+				// parse stage definition for sed
 				{StageNo: 2, IsStage: true},
 				{StageNo: 2, Str: testutil.Ptr("sed")},
-				// -cmd "sed"
+				// parse command sed
 				{StageNo: 2, IsCmd: true},
 				{StageNo: 2, Str: testutil.Ptr("sed")},
-				// --opt "e"
+				// parse option e for sed
 				{StageNo: 2, IsOpt: true},
 				{StageNo: 2, Str: testutil.Ptr("e")},
-				// --arg "/aa/bb/"
+				// parse argument expression for sed
 				{StageNo: 2, IsArg: true},
 				{StageNo: 2, Str: testutil.Ptr("/aa/bb/")},
 			},
