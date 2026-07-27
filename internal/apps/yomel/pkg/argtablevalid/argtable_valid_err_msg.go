92fe6f777d12cbd7299e9bba1bf20eb17b7bfa9e
puutaro <hoperstep@gmail.com>
2026-07-27 22:30:25 +0900

    ajust validate err msg

--- internal/apps/yomel/pkg/argtablevalid/argtable_valid_err_msg.go
+++ internal/apps/yomel/pkg/argtablevalid/argtable_valid_err_msg.go
@@ -5,16 +5,18 @@ import (
 )
 
 const (
-	stageNotFound                = "'" + argtables.StageSignal + "'" + " not found"
-	cmdOpNameWithQuote           = "'" + argtables.CmdOpSignal + "'"
-	cmdNotFound                  = cmdOpNameWithQuote + " not found\nstageNo: %d"
-	cmdDuplidate                 = cmdOpNameWithQuote + " duplication\nstageNo: %d"
-	svcOpNameWithQuote           = "'" + argtables.SvcOpSignal + "'"
-	actOpNameWithQuote           = "'" + argtables.ActOpSignal + "'"
-	cmdSvcActOrdrerIrregular     = "Must be" + cmdOpNameWithQuote + "->" + svcOpNameWithQuote + "->" + actOpNameWithQuote + "order \nstageNo: %d"
-	argOpSignalWithQuote         = "'" + argtables.ArgOpSignal + "'"
-	optOpSignalWithQuote         = "'" + argtables.OptOpSignal + "'"
-	singleOpSignalWithQuote      = "'" + argtables.SingleOpSignal + "/" + argtables.SingleShortOpSignal + "'"
-	noQuoteOpSignalWithQuote     = "'" + argtables.NoQuoteOpSignal + "/" + argtables.NoQuoteShortOpSignal + "'"
-	quoteOptionIrregularPosition = singleOpSignalWithQuote + " and " + noQuoteOpSignalWithQuote + " must be immediately after " + argOpSignalWithQuote + " and " + optOpSignalWithQuote
+	stageNotFound            = "'" + argtables.StageSignal + "'" + " not found"
+	cmdOpNameWithQuote       = "'" + argtables.CmdOpSignal + "'"
+	cmdNotFound              = cmdOpNameWithQuote + " not found\nstageNo: %d"
+	cmdDuplidate             = cmdOpNameWithQuote + " duplication\nstageNo: %d"
+	svcOpNameWithQuote       = "'" + argtables.SvcOpSignal + "'"
+	actOpNameWithQuote       = "'" + argtables.ActOpSignal + "'"
+	cmdSvcActOrdrerIrregular = "Must be" + cmdOpNameWithQuote + "->" + svcOpNameWithQuote + "->" + actOpNameWithQuote + "order \nstageNo: %d"
+
+	argOpSignalWithQuote     = "'" + argtables.ArgOpSignal + "'"
+	optOpSignalWithQuote     = "'" + argtables.OptOpSignal + "'"
+	singleOpSignalWithQuote  = "'" + argtables.SingleOpSignal + "/" + argtables.SingleShortOpSignal + "'"
+	noQuoteOpSignalWithQuote = "'" + argtables.NoQuoteOpSignal + "/" + argtables.NoQuoteShortOpSignal + "'"
+	// Include duplicate quoteOption specifed
+	quoteOptionIrregularPosition = singleOpSignalWithQuote + " and " + noQuoteOpSignalWithQuote + " must be immediately after " + argOpSignalWithQuote + " and " + optOpSignalWithQuote + "\nstageNo: %d"
 )
--- internal/apps/yomel/pkg/modelvalid/model_err_msg.go
+++ internal/apps/yomel/pkg/modelvalid/model_err_msg.go
@@ -4,7 +4,8 @@ import "github.com/puutaro/yomel/internal/apps/yomel/pkg/argtables"
 
 const (
 	stageDescriptionIrregular = "'" + argtables.StageSignal + "'" + " description must be meaning sentence\ndesc: '%s', stageNo: %d"
-	cmdStrNotFound            = "'" + argtables.CmdOpSignal + "'" + " value is require\ndesc: '%s', stageNo: %d"
-	svcStrNotFound            = "'" + argtables.SvcOpSignal + "'" + " value is require\ndesc: '%s', stageNo: %d"
-	actStrNotFound            = "'" + argtables.ActOpSignal + "'" + " value is require\ndesc: '%s', stageNo: %d"
+
+	cmdStrNotFound = "'" + argtables.CmdOpSignal + "'" + " value is required\nstageNo: %d"
+	svcStrNotFound = "'" + argtables.SvcOpSignal + "'" + " value is required\nstageNo: %d"
+	actStrNotFound = "'" + argtables.ActOpSignal + "'" + " value is required\nstageNo: %d"
 )
