1b7022198a34271e71681972711f4413c8728833
puutaro <hoperstep@gmail.com>
2026-07-30 22:17:06 +0900

    add help test

--- internal/apps/yomel/pkg/info/help_test.go
+++ internal/apps/yomel/pkg/info/help_test.go
@@ -1,1 +1,88 @@
+// Write direct above line for Comment on code
 package info
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/BurntSushi/toml"
+	"github.com/puutaro/yomel/internal/apps/yomel/pkg/argtables"
+	"github.com/puutaro/yomel/internal/pkg/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetHelpByDefault(t *testing.T) {
+	var info YomelInfo
+	toml.Decode(YomelInfoRaw, &info)
+	description := info.Yomel.Description
+	tests := []struct {
+		name      string
+		input     []string
+		wantHelp  *string
+		wantError error
+	}{
+		{
+			name:  "should return help content when help flag is present in control section",
+			input: []string{},
+			wantHelp: testutil.Ptr(
+				strings.Join(
+					[]string{description, "", detail},
+					"\n",
+				),
+			),
+			wantError: nil,
+		},
+		{
+			name:      "should return nil when help flag is not present",
+			input:     []string{"--log"},
+			wantHelp:  nil,
+			wantError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotHelp, gotErr := GetHelpByDefault(tt.input)
+			if tt.wantError != nil {
+				assert.EqualError(t, gotErr, tt.wantError.Error())
+			} else {
+				assert.NoError(t, gotErr)
+			}
+			if tt.wantHelp != nil {
+				assert.Equal(t, *tt.wantHelp, *gotHelp)
+			} else {
+				assert.Equal(t, tt.wantHelp, gotHelp)
+			}
+		})
+	}
+}
+
+func Test_GetHelpByOption(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     []argtables.ArgTable
+		wantHelp  *string
+		wantError error
+	}{
+		{
+			name: "should return nil when help option is not triggered",
+			input: []argtables.ArgTable{
+				{StageNo: 1, IsStage: true},
+			},
+			wantHelp:  nil,
+			wantError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotHelp, gotErr := GetHelpByOption(tt.input)
+			if tt.wantError != nil {
+				assert.EqualError(t, gotErr, tt.wantError.Error())
+			} else {
+				assert.NoError(t, gotErr)
+			}
+			assert.Equal(t, tt.wantHelp, gotHelp)
+		})
+	}
+}
