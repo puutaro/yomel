6256c5bfbfcc7df4575c3e8dfbee205b3c08f611
puutaro <hoperstep@gmail.com>
2026-07-30 22:56:29 +0900

    add arg_table_valid_checkCtrlParameterSpecifyInStageErr_test.go

--- internal/apps/yomel/pkg/argtablevalid/arg_table_valid_checkCtrlParameterSpecifyInStageErr_test.go
+++ internal/apps/yomel/pkg/argtablevalid/arg_table_valid_checkCtrlParameterSpecifyInStageErr_test.go
@@ -1,1 +1,55 @@
 package argtablevalid
+
+import (
+	"testing"
+
+	"github.com/puutaro/yomel/internal/apps/yomel/pkg/argtables"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_checkCtrlParameterSpecifyInStageErr(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     []argtables.ArgTable
+		wantError string
+	}{
+		{
+			name: "should return nil when control parameters are not specified in stage fields",
+			input: []argtables.ArgTable{
+				{StageNo: 0, IsVersion: true},
+				{StageNo: 0, IsHelp: true},
+				{StageNo: 1, IsStage: true},
+				{StageNo: 1, IsCmd: true},
+				{StageNo: 1, Str: testutilPtr("echo")},
+			},
+			wantError: "",
+		},
+		{
+			name: "should return error when version is specified in stage field",
+			input: []argtables.ArgTable{
+				{StageNo: 1, IsStage: true},
+				{StageNo: 1, IsVersion: true},
+			},
+			wantError: "must be specified'--version' and '--help' in stage 0 field\nstageNo: 1",
+		},
+		{
+			name: "should return error when help is specified in stage field",
+			input: []argtables.ArgTable{
+				{StageNo: 1, IsStage: true},
+				{StageNo: 1, IsHelp: true},
+			},
+			wantError: "must be specified'--version' and '--help' in stage 0 field\nstageNo: 1",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := checkCtrlParameterSpecifyInStageErr(tt.input)
+			if tt.wantError == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, tt.wantError)
+			}
+		})
+	}
+}
