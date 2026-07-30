ad9db67b20a844d2e11fe65c86a856511003c48d
puutaro <hoperstep@gmail.com>
2026-07-30 22:58:48 +0900

    add arg_table_valid_checkOnlyOneOptionErr_test.go

--- internal/apps/yomel/pkg/argtablevalid/arg_table_valid_checkOnlyOneOptionErr_test.go
+++ internal/apps/yomel/pkg/argtablevalid/arg_table_valid_checkOnlyOneOptionErr_test.go
@@ -1,1 +1,46 @@
 package argtablevalid
+
+import (
+	"testing"
+
+	"github.com/puutaro/yomel/internal/apps/yomel/pkg/argtables"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_checkOnlyOneOptionErr(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     []argtables.ArgTable
+		wantError string
+	}{
+		{
+			name: "should return nil when options appear only once",
+			input: []argtables.ArgTable{
+				{StageNo: 1, IsStage: true},
+				{StageNo: 1, IsCmd: true},
+				{StageNo: 1, IsOpt: true},
+			},
+			wantError: "",
+		},
+		{
+			name: "should return error when cmd is duplicated in a single stage",
+			input: []argtables.ArgTable{
+				{StageNo: 1, IsStage: true},
+				{StageNo: 1, IsCmd: true},
+				{StageNo: 1, IsCmd: true},
+			},
+			wantError: "'-cmd' are only one in each stage field\nstageNo: 1",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := checkOnlyOneOptionErr(tt.input)
+			if tt.wantError == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, tt.wantError)
+			}
+		})
+	}
+}
