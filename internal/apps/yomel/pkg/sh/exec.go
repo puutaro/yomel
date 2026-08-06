a852a68c4156ff3e221cdd27834618c7fcdc263b
puutaro <hoperstep@gmail.com>
2026-08-06 15:36:26 +0900

    output log when has err always

--- internal/apps/yomel/pkg/sh/exec.go
+++ internal/apps/yomel/pkg/sh/exec.go
@@ -88,10 +88,9 @@ func Exec(yomelInfo YomelInfo) {
 	// 6. Finally, output decorated logs to os.Stderr based on flag conditions
 	for i, stageInfo := range stageInfos {
 		stdoutLen := stdoutBuffers[i].Len()
-		stderrLen := stderrBuffers[i].Len()
 
 		shouldLog := (stageInfo.IsLog && stdoutLen > 0) ||
-			(stderrLen > 0 && cmdHasError)
+			cmdHasError
 
 		if !cmdHasError && !shouldLog {
 			continue
